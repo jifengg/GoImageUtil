6cf31cd8a51b9cf86630f4b5047da7fd59a66763
季风 <11-dongdong@163.com>
2019-03-14 15:36:23 +0800

    首次提交

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # GoImageUtil
-A image util for Golang
+A image command line tool util for Golang。
+
+对常用图像处理命令行工具的golang封装。
+
+目前使用[ImageMagick](https://github.com/ImageMagick/ImageMagick)来进行图片的裁剪、格式转换、压缩（非PNG）。
+使用[pngquant](https://github.com/kornelski/pngquant)来进行PNG图片压缩。
+
+要使用这个包，请保证系统中已经安装了ImageMagick和pngquant。安装方式请自行查找。
+
+
+------
+
+# install   安装
+
+## go package
+
+```shell
+go get github.com/jifengg/GoImageUtil
+```
+
+# usage 使用
+
+可参考[`test/test.go`](./test/test.go)；
+
+```go
+import (
+	iu "githup.com/jifengg/GoImageUtil"
+)
+//修改命令行工具的位置，可以使用相对路径，也可以使用绝对路径。
+//Init并非必须要调用的方法，但是可以使用此方法测试命令行工具是否可用。
+err := iu.Init(conf)
+
+//获取图片信息
+info, err := iu.Info(testFile)
+if err != nil {
+    fmt.Printf("info error:%s\n", err)
+} else {
+    fmt.Printf("info:%+v\n", info)
+}
+/*
+{
+    "IsKnowImage": true,
+    "Width": 573,
+    "Height": 573,
+    "FileSize": 81166,
+    "FilePath": "./test/test_image/me.jpg",
+    "Format": "JPEG"
+}
+*/
+
+
+//转换一个jpg文件，生成一个300x200，质量参数为60的jpg文件
+opt := iu.Option{
+    Quality: 60,
+    Width:   300,
+    Heigth:  200}
+succ, err := iu.Convert(testFile, path.Join(tempDir, "out.jpg"), opt)
+
+
+
+//转换一个jpg文件成png文件，不修改分辨率，pngquant压缩参数为40-80
+opt = iu.Option{
+    Quality:       80,
+    PngQunlityMin: 40}
+//转换一个jpg文件成png文件，不修改分辨率，pngquant压缩参数为40-80
+succ, err = iu.Convert(testFile, path.Join(tempDir, "out.png"), opt)
+
+```
+
+# test 测试
+
+```shell
+go run test\test.go
+```
+
+运行之后，如果没有报错，可以到`test/test_output/`中查看输出的文件。
---
+++ main.go
@@ -0,0 +1,212 @@
+/**
+使用多个命令行工具，进行图片相关的处理
+*/
+
+package goimageutil
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+// imageMagickConvertPath ImageMagick 包的 convert 工具的路径
+var imageMagickConvertPath = "convert"
+
+// imageMagickIdentifyPath ImageMagick 包的 identify 工具的路径
+var imageMagickIdentifyPath = "identify"
+
+// pngquantPath pngquant命令行工具的路径
+var pngquantPath = "pngquant"
+
+// 是否打印出调试信息
+var showDebug = false
+
+// 是否打印出错误信息
+var showError = false
+
+// JPEG 图片格式JPEG
+var JPEG = "JPEG"
+
+// PNG 图片格式PNG
+var PNG = "PNG"
+
+// GIF 图片格式GIF
+var GIF = "GIF"
+
+// BMP 图片格式BMP
+var BMP = "BMP"
+
+// ImageInfo 图片信息
+type ImageInfo struct {
+	IsKnowImage bool   `json:"isimg"` // 是否是已知的图片格式
+	Width       uint   `json:"w"`     // 宽度像素
+	Height      uint   `json:"h"`
+	FileSize    int64  `json:"size"`
+	FilePath    string `json:"path"`
+	Format      string `json:"m"`
+}
+
+// Option 处理选项
+type Option struct {
+	Width         uint // 要缩放成的宽度，为0表示默认
+	Heigth        uint // 要缩放成的高度，为0表示默认
+	Quality       uint // 不等于0时，表示要压缩，设置图片的质量参数，1-100。如果要压缩的是png格式，则这个值表示pngquant的质量参数的最大值
+	PngQunlityMin uint // pngquant压缩png图片时，质量参数的最小值
+}
+
+// Config 工具的配置信息
+type Config struct {
+	ImageMagickConvertPath  string //ImageMagick 包的 convert 工具的路径
+	ImageMagickIdentifyPath string //ImageMagick 包的 identify 工具的路径
+	PngquantPath            string //pngquant命令行工具的路径
+	ShowDebug               bool   //是否打印出调试信息
+	ShowError               bool   //是否打印出错误信息
+}
+
+// Init 进行初始化，检查各个命令是否可用。
+func Init(conf Config) error {
+	exitCode, _, err := run(conf.ImageMagickConvertPath, "--version")
+	if exitCode != 0 || err != nil {
+		return err
+	}
+	exitCode, _, err = run(conf.ImageMagickIdentifyPath, "--version")
+	if exitCode != 0 || err != nil {
+		return err
+	}
+	exitCode, _, err = run(conf.PngquantPath, "--version")
+	if exitCode != 0 || err != nil {
+		return err
+	}
+
+	imageMagickConvertPath = conf.ImageMagickConvertPath
+	imageMagickIdentifyPath = conf.ImageMagickIdentifyPath
+	pngquantPath = conf.PngquantPath
+	showDebug = conf.ShowDebug
+	showError = conf.ShowError
+	if showDebug {
+		fmt.Printf("ImageMagickConvertPath set to :%s\n", imageMagickConvertPath)
+		fmt.Printf("ImageMagickIdentifyPath set to :%s\n", imageMagickIdentifyPath)
+		fmt.Printf("PngquantPath set to :%s\n", pngquantPath)
+		fmt.Printf("ShowError set to :%t\n", showError)
+	}
+	return nil
+}
+
+// run 执行一个外部程序，返回：退出码，输出，异常
+func run(name string, arg ...string) (int, []byte, error) {
+	if showDebug {
+		fmt.Println(name, arg)
+	}
+	cmd := exec.Command(name, arg...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return 1, nil, err
+	}
+	exitCode := (cmd.ProcessState.Sys().(syscall.WaitStatus)).ExitStatus()
+
+	return exitCode, output, nil
+}
+
+// Info 获取图片的相关信息
+func Info(file string) (ImageInfo, error) {
+	info := ImageInfo{}
+	fi, err := os.Stat(file)
+	if err == nil {
+		format := `{"w":%[w],"h":%[h],"m":"%[m]"}` //,"size":%[B]}	B=文件字节数，在7.0版本之前不支持
+		exitCode, output, runerr := run(imageMagickIdentifyPath, "-format", format, file)
+		if exitCode == 0 {
+			if showDebug {
+				fmt.Printf("get info output:%s\n", output)
+			}
+			err = json.Unmarshal(output, &info)
+			info.FileSize = fi.Size()
+			info.IsKnowImage = true
+			info.FilePath = file
+		} else {
+			info.IsKnowImage = false
+			err = runerr
+		}
+	}
+	if err != nil && showError {
+		fmt.Printf("get info error:%s\n", err)
+	}
+	return info, err
+}
+
+// Convert 图片转换
+// fileIn 要处理的文件的绝对路径
+func Convert(fileIn string, fileOut string, opt Option) (bool, error) {
+	var args []string
+	//处理过程中使用的临时文件
+	tempFile := fileIn
+	outFormat := strings.ToUpper(strings.Replace(path.Ext(fileOut), ".", "", -1))
+	info, err := Info(fileIn)
+	if err != nil {
+		return false, err
+	}
+	if info.IsKnowImage == false {
+		return false, errors.New("UnknowImageFormat")
+	}
+	//如果要修改分辨率
+	if opt.Width > 0 || opt.Heigth > 0 {
+		var w, h uint = 0, 0
+		if opt.Width == 0 {
+			h = opt.Heigth
+			w = info.Width * h / info.Height
+		} else if opt.Heigth == 0 {
+			w = opt.Width
+			h = info.Height * w / info.Width
+		} else {
+			w = opt.Width
+			h = opt.Heigth
+		}
+		//加上感叹号!表示强制转换到这个分辨率，否则会按照等比例缩放
+		args = append(args, "-resize", fmt.Sprintf("%dx%d!", w, h))
+	}
+	//如果需要进行压缩
+	if opt.Quality > 0 && outFormat != PNG {
+		args = append(args, "-quality", strconv.Itoa(int(opt.Quality)))
+	}
+	//如果没有特殊参数，判断输入输出文件格式是否相同，如果相同，则不做处理
+	if len(args) > 0 || path.Ext(tempFile) != path.Ext(fileOut) {
+		args = append(args, tempFile, fileOut)
+	}
+
+	//如果需要使用ImageMagick处理
+	if len(args) > 0 {
+		exitCode, output, err := run(imageMagickConvertPath, args...)
+		if exitCode != 0 {
+			return false, err
+		}
+		if showDebug {
+			fmt.Printf("ImageMagick exit with code (%d)\noutput: %s\nerror:%s\n", exitCode, output, err)
+		}
+		//将临时文件路径指向输出文件，便于后续处理
+		tempFile = fileOut
+	}
+
+	//如果是png，且需要压缩
+	if opt.Quality > 0 && outFormat == PNG {
+		if opt.PngQunlityMin > opt.Quality {
+			opt.PngQunlityMin = opt.Quality
+		}
+		args = []string{
+			"--force", "--quiet", "--ordered", "--speed=1", fmt.Sprintf("--quality=%d-%d", opt.PngQunlityMin, opt.Quality), tempFile, "--output", fileOut}
+		exitCode, output, err := run(pngquantPath, args...)
+		if showDebug {
+			fmt.Printf("Pngquant exit with code (%d)\noutput: %s\nerror:%s\n", exitCode, output, err)
+		}
+		if exitCode != 0 {
+			return false, err
+		}
+	}
+
+	return true, nil
+}
